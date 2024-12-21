c8d40d08a9fcb057b6f38e4254df142ae285210f
idletext <210270082+idletext@users.noreply.github.com>
2024-12-21 17:59:21 +0800

    Add first function in 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,16 @@
-const repeatString = function() {
-// something something something
-// Edit something again. 
-// git edit something yadda yadda
+const repeatString = function(message, num) {
+// hey*3
+// initializer is calls the function and serves
+// as counter
+// Condition if init = 4 stop looping
+//
+    for (let i = 0, i < (num + 1), i++) {
+        alert (message);
+    }
 
-// Did another change
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
