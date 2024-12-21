11d17093343b9d7f613604a4ca0c5f89c5016bbf
idletext <210270082+idletext@users.noreply.github.com>
2024-12-21 20:50:36 +0800

    Remove accidental copy of javascript repo

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,15 @@
-const repeatString = function(message, num) {
+const repeatString = function(string, num) {
 // hey*3
 // initializer is calls the function and serves
 // as counter
 // Condition if init = 4 stop looping
 //
-    for (let i = 0, i < (num + 1), i++) {
-        alert (message);
+    for (let i = 0; i < (num + 1); i++) {
+        return (string);
     }
+};  
 
-};
-
-repeatString('hey', 3)
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
