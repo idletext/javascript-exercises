2b206b493b6031fddd9744157b87aea8c2a18c23
idletext <210270082+idletext@users.noreply.github.com>
2025-01-09 16:56:45 +0800

    Finished 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,20 @@
-const removeFromArray = function(ary, val) {
+const removeFromArray = function(ary, ...val) {
  
-    let i = 0;
-    while (i < ary.length) {
-        if (ary[i] === val) {
-            ary.splice(i, 1)
-        } else {
-            ++i;
-        }
-    }
-    return ary;
+    // let i = 0;
+    // while (i < ary.length) {
+    //     if (ary[i] === val) {
+    //         ary.splice(i, 1);
+    //     } else {
+    //         ++i;
+    //     }
+    // }
+    // return ary;
+
+    return ary.filter(x => !val.includes(x))
+
 };
 
-
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
