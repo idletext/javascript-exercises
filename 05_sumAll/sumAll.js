bd657a74f4e97d57a16c854067f5ffdad6f09797
idletext <210270082+idletext@users.noreply.github.com>
2025-01-16 17:01:04 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,46 @@
 const sumAll = function(val1, val2) {
-    let finAry = 0
-
+    let finAry = [];
+    let sum = 0;
     // an if statement here that changes
     // the value of the final 
-    for (; i < val2 + 1; i++)    
+    
+    if ( (val1) < 0 ||
+         (val2) < 0 || 
+        //  (val1||val2) !== 0 ||
+        //  typeof 0 !== typeof (val1||val2)||
+        //  !Number.isNumber((val1||val2))||
+        //  Number.isNaN(val1||val2)||
+        //  Array.isArray(val1||val2)||
+         !Number.isInteger(val1)||
+         !Number.isInteger(val2)){
+            return "ERROR";
+    }else if (val1 < val2) {
+        for ( let i = val1; i <= val2; i++) {
+            finAry.push(i);
+        };
+    }else if (val1 > val2){
+        for ( let i = val1; i >= val2; i--) {
+            finAry.push(i);
+        };
+    } 
+
+    // } else if (val1 < 0){
+    //     sum.push('ERROR');
+    //     sumAll.break;   
+    // }
+
+    finAry.forEach(x => {
+        sum += x;
+    });
+    
+    if (sum > 0){
+        return sum;
+    };
+
+};
+
+
+
 
     //create a loop that loops through the given numbers.
     
@@ -16,8 +53,6 @@ const sumAll = function(val1, val2) {
 
 // return the sum of the finAry variable when loop finishes.
 
-};
-
-sumAll(1, 4)
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
