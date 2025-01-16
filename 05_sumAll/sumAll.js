fcd2c80fb0580deaf7eec478bcd3b7266c6ff36e
idletext <210270082+idletext@users.noreply.github.com>
2025-01-16 12:38:02 +0800

    Add 05 exercise progress

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    let finAry = 0
+
+    // an if statement here that changes
+    // the value of the final 
+    for (; i < val2 + 1; i++)    
+
+    //create a loop that loops through the given numbers.
+    
+    //get all numbers that are inside 2 integers
+    // start a sum loop from the start to finish element
+    // if a large number is given first, reverse the sum loop
+    // give out an error if the number is not an integer.
+
+    // the values of the forloop could be the argument value
+
+// return the sum of the finAry variable when loop finishes.
 
 };
 
+sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
