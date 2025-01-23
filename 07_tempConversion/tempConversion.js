f4e0ec9b112ff2f108ed2aecf064338b9ea44d4c
idletext <210270082+idletext@users.noreply.github.com>
2025-01-23 14:53:26 +0800

    Finish 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+
+//convert celcius to fahrenheit
+//let cnvCel
+
+//if converted celcius is not rounded, round
+//else return cnv
+
+// if number is an integer, return number
+// else, convert 
+
+  //  let cnvCel = 0;
+   const compToCel = (fah - 32) * 5/9 ;
+   let rndFah = Math.round(compToCel *10) / 10;
+   return rndFah;
+  
+  };
+
+
+   const convertToFahrenheit = function(cel) {
+    const compToFah = (cel * 9 / 5) + 32 ;
+    let rndCel = Math.round(compToFah *10) / 10;
+    return rndCel;
 };
 
-const convertToFahrenheit = function() {
-};
 
+convertToCelsius()
+convertToFahrenheit()
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
