78bc37681ec22e76d7d1ec7337ce646c2f7fcb60
idletext <210270082+idletext@users.noreply.github.com>
2025-01-23 12:28:18 +0800

    Test x13 commit

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
 const leapYears = function() {
-
+    if (leap year){
+        return "true";
+    }else {
+        return "false";
+    }
 };
 
+leapYears()
 // Do not edit below this line
 module.exports = leapYears;
