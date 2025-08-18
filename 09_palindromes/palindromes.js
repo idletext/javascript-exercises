cee32aa73a4d39b25a8734435345ac77a29ddc18
idletext <210270082+idletext@users.noreply.github.com>
2025-08-18 12:09:22 +0800

    Finish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+let punct = ["!", "?", ",", " ","."];
+
+let filterStr = str
+.toLowerCase()
+.split("")
+.filter(x => !punct.includes(x))
+.join("");
+
+let revStr = "";
+
+for (let i = filterStr.length - 1; i >= 0; i--){
+    revStr += filterStr[i];
+};
+
+return (revStr == filterStr) ?  true :  false;
 
 };
 
