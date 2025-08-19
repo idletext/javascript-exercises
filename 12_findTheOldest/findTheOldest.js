99fa64d03d9801ce4322d40fe8e14be90b869607
idletext <210270082+idletext@users.noreply.github.com>
2025-08-19 16:00:56 +0800

    Feature 12 added another variable that inserts yearOfDeath property
    - Tested it with console.log. Need to clean after.
    - Tried placing the yOD inserter inside the .reduce variable but I wasn't able to
    make it work.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function(human) {
+let people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+];
 
-    let getOldest = human.reduce((obj, value) => {
+const findTheOldest = function(human) {
+    
+    let addYod = human.map(x => {
+        let date = new Date();
+
+        if (!("yearOfDeath" in x) ) {
+            x.yearOfDeath = date.getFullYear();
+        }
+        return x;
+    });
+    
+
+    let getOldest = addYod.reduce((obj, value) => {
+
+        // let yod = value["yearOfDeath"];
+        // if (value.find(x => yod.includes(x))) value.push(yod)
+
+        // if ( !("yearOfDeath" in value) ) {
+        //     value.yearOfDeath = "2025";
+        // };
 
         if (  
             (obj['yearOfDeath'] - obj['yearOfBirth']) 
@@ -13,8 +47,10 @@ const findTheOldest = function(human) {
 
     });
 
+
     return(getOldest);
 };
 
+console.log(findTheOldest(people))
 // Do not edit below this line
-module.exports = findTheOldest;
+// module.exports = findTheOldest;
