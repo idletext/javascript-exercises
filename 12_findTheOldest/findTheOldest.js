b758b28789ff10b3a33b760fb904324da06612dd
idletext <210270082+idletext@users.noreply.github.com>
2025-08-19 14:12:45 +0800

    Feature 12_findTheOldest made cleaner array "arrAge" variable
    - Variable that uses .map to make a new array with just {name:, age:}
    - Also made "getOldest" variable with .reduce to find the oldest of "arrAge",
     but it still doesn't work right

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+
+const findTheOldest = function(human) {
+    // let arrAge = [];
+
+    let arrAge = human.map(user => ({
+        name: `${user.name}`,
+        age: `${user.yearOfDeath - user.yearOfBirth}`
+    }));
+
+    let getOldest = arrAge.reduce((obj, value) => {
+        let getAge = [obj.age];
+
+        if (getAge < value.age){
+            return getAge = value.age; 
+        } else {
+            return getAge;
+        };
+
+    });
+
+    return(getOldest)
 };
 
+console.log(findTheOldest(people))
+
 // Do not edit below this line
-module.exports = findTheOldest;
+// module.exports = findTheOldest;
