ee7e844a6dff2769c402964f72b9da77df82874e
idletext <210270082+idletext@users.noreply.github.com>
2025-08-19 16:13:02 +0800

    Finish 12_findTheOldest!
    - :D

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,3 @@
-let people = [
-    {
-      name: "Carly",
-      yearOfBirth: 2018,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-];
-
 const findTheOldest = function(human) {
     
     let addYod = human.map(x => {
@@ -26,16 +9,7 @@ const findTheOldest = function(human) {
         return x;
     });
     
-
     let getOldest = addYod.reduce((obj, value) => {
-
-        // let yod = value["yearOfDeath"];
-        // if (value.find(x => yod.includes(x))) value.push(yod)
-
-        // if ( !("yearOfDeath" in value) ) {
-        //     value.yearOfDeath = "2025";
-        // };
-
         if (  
             (obj['yearOfDeath'] - obj['yearOfBirth']) 
             < (value['yearOfDeath'] - value['yearOfBirth'])
@@ -44,13 +18,10 @@ const findTheOldest = function(human) {
         } else {
             return obj;
         };
-
     });
-
 
     return(getOldest);
 };
 
-console.log(findTheOldest(people))
 // Do not edit below this line
-// module.exports = findTheOldest;
+module.exports = findTheOldest;
